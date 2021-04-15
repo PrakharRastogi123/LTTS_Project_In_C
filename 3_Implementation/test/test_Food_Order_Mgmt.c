ac5a2cd3d0f87b3b4549da7d36a0a421d8e35bc9
Prakhar Rastogi <prkharrastogi99.pr@gmail.com>
2021-04-15 21:29:40 +0530

    Update test_Food_Order_Mgmt.c

--- 3_Implementation/test/test_Food_Order_Mgmt.c
+++ 3_Implementation/test/test_Food_Order_Mgmt.c
@@ -13,7 +13,11 @@ void setUp()
 void tearDown()
 {
 }
-
+void test_insert1(){
+  head startorder;
+  startorder.start=NULL;
+  TEST_ASSERT_EQUAL(SUCCESS,insertend(&startorder,1.1,"Cola",1,20));
+}
 
 void test_FoodList(){
   head foodstart;
@@ -21,12 +25,20 @@ void test_FoodList(){
   TEST_ASSERT_EQUAL(ERROR_NULL_PTR,foodlist(&foodstart));
 }
 
-void test_Insert(){
+void test_FoodList2(){
+  head startorder;
+  startorder.start=NULL;
+  insertend(&startorder, 1.1, "Samosa", 2, 50);
+  insertend(&startorder, 1.2, "Grilled Cheese Sandwich", 1, 100);
+  TEST_ASSERT_EQUAL(SUCCESS,foodlist(&startorder));
+}
+
+void test_Insert2(){
   head foodstart;
   foodstart.start=NULL;
   TEST_ASSERT_EQUAL(SUCCESS,insertend(&foodstart,1.0,"Aloo Paratha",2,50));
 }
-void test_getBill(){
+void test_getBill1(){
       head startorder;
       startorder.start=NULL;
       insertend(&startorder, 1.1, "Samosa", 2, 50);
@@ -35,6 +47,14 @@ void test_getBill(){
       TEST_ASSERT_EQUAL(200,getbill(&startorder));
 }
 
+void test_getBill2(){
+      head startorder;
+      startorder.start=NULL;
+      insertend(&startorder, 2.1, "IceCream", 2, 50);
+      insertend(&startorder, 2.2, "Grilled Cheese Wrap", 1, 200);
+      //printf("\n%d\n",foodlist(&startorder));
+      TEST_ASSERT_EQUAL(300,getbill(&startorder));
+}
     
 
 
@@ -45,8 +65,11 @@ int main(void)
 
     /* Run Test functions */
     RUN_TEST(test_FoodList);
-    RUN_TEST(test_Insert);
-    RUN_TEST(test_getBill);
+    RUN_TEST(test_Insert2);
+    RUN_TEST(test_getBill1);
+    RUN_TEST(test_getBill2);
+    RUN_TEST(test_insert1);
+    RUN_TEST(test_FoodList2);
     
     /* Close the Unity Test Framework */
     return UNITY_END();
